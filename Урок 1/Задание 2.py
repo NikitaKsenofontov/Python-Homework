6692779133387fa4a0e8eff6955f16e2ab59c862
Nikita Ksenofontov <ms.ksenofontov@mail.ru>
2020-02-19 02:39:37 +0300

    Решение задания 4 и доработки по предыдущим заданиям

--- Урок 1/Задание 2.py
+++ Урок 1/Задание 2.py
@@ -1,7 +1,10 @@
 user_time = int(input("Пожалуйста укажите время в секундах: "))
 
+while user_time < 0:
+    user_time = int(input("Отрицательного времени не бывает: "))
+
 hours = user_time // 3600
 minutes = user_time // 60
 seconds = user_time - minutes * 60
 
-print('%02d:%02d:%02d' % (hours, minutes, seconds))
+print('Время: %02d:%02d:%02d' % (hours, minutes, seconds))
--- Урок 1/Задание 3.py
+++ Урок 1/Задание 3.py
@@ -1,5 +1,8 @@
 user_number = input("Введите число: ")
 
+while int(user_number) < 0:
+    user_number = input("Введите положительное число: ")
+
 result = int(user_number) + int(user_number + user_number) + int(user_number + user_number + user_number)
 
 print(f'Результат вычислений: {result}')
