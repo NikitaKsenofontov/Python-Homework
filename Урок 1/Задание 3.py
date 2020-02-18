474fbd5272e500ee15fd94135515fefbf81a9f86
Nikita Ksenofontov <ms.ksenofontov@mail.ru>
2020-02-18 23:31:15 +0300

    Решение задания 3

---
+++ Урок 1/Задание 3.py
@@ -0,0 +1,5 @@
+user_number = input("Введите число: ")
+
+result = int(user_number) + int(user_number + user_number) + int(user_number + user_number + user_number)
+
+print(f'Результат вычислений: {result}')
