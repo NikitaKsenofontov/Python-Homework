d09baf9c98a5919b2d97f98b7e48fc1a96873330
Nikita Ksenofontov <ms.ksenofontov@mail.ru>
2020-02-19 23:33:33 +0300

    Создание папки и ветки для проверки ДЗ

---
+++ Урок 1/Проверка ДЗ/Задание 1.py
@@ -0,0 +1,12 @@
+number_1 = int(input('Введите число №1: '))
+number_2 = int(input('Введите число №2: '))
+
+print(number_1)
+print(type(number_1))
+print(number_2)
+print(type(number_2))
+
+city = input("Укажите город в котором проживаете: ")
+
+print(city)
+print(type(city))
