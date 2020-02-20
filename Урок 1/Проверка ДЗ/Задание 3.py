2906870fb8e3c9efd952bba606c8dbca36055fbb
Nikita Ksenofontov <ms.ksenofontov@mail.ru>
2020-02-20 22:57:14 +0300

    Создание папки урок 2

--- Урок 1/Проверка ДЗ/Задание 3.py
+++
@@ -1,8 +0,0 @@
-user_number = input("Введите число: ")
-
-while int(user_number) < 0:
-    user_number = input("Введите положительное число: ")
-
-result = int(user_number) + int(user_number + user_number) + int(user_number + user_number + user_number)
-
-print(f'Результат вычислений: {result}')
