319fa4fff975bd960730f820f416f0ecda199736
Nikita Ksenofontov <ms.ksenofontov@mail.ru>
2020-02-25 21:46:23 +0300

    Переход на Урок 3

--- Урок 2/Проверка ДЗ/Проверка ДЗ/Задание 4.py
+++
@@ -1,6 +0,0 @@
-my_string = input('Пожалуйста введите текст из разных слов (без знаков препинания): ')
-
-my_list = my_string.split()
-
-for num, i in enumerate(my_list):
-    print(f'{num + 1} - {i[:10]}') if len(i) > 10 else print(f'{num + 1} - {i}')
