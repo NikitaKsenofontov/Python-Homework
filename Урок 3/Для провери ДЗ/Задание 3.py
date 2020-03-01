df97d75f2763fb2f672eab192481b89d867f3011
Nikita Ksenofontov <ms.ksenofontov@mail.ru>
2020-03-01 15:20:27 +0300

    Удаление ветки

--- Урок 3/Для провери ДЗ/Задание 3.py
+++
@@ -1,7 +0,0 @@
-def division(numbers):
-    numbers = list(map(int, numbers))
-    return sum(numbers) - min(numbers)
-
-
-user_numbers = input('Введите три числа через пробел: ').split()
-print(f'Сумма наибольших двух чисел: {division(user_numbers)}')
