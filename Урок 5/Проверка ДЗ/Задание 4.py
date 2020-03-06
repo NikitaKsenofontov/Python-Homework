93d4be7f829b49ee4b5eaa49f3a2f0e354c95ef7
Nikita Ksenofontov <ms.ksenofontov@mail.ru>
2020-03-06 23:27:14 +0300

    Завершение проверки ДЗ по Уроку 5

--- Урок 5/Проверка ДЗ/Задание 4.py
+++
@@ -1,8 +0,0 @@
-with open('Translated_numbers', 'w', encoding='utf-8') as user_translate:
-    print(f'Файл {user_translate.name} готов к редактированию.')
-    with open('Numbers.txt', 'r') as numbers:
-        for line in numbers:
-            translation = lambda *args: input(f'Введите перевод слова {args[0][0]}: ')
-            new_word = translation(line.split())
-            user_translate.writelines(line.replace(line.split()[0], new_word))
-    print(f'Редактирование файла {user_translate.name} Завершено.')
